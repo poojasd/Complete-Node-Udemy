3cc1bad40479cb08f41ca5b3031584312a961f7d
pooja <pooja@fenixwork.com>
2018-09-20 12:33:27 +0530

    require notes export function

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
+var res = notes.addNote();
+console.log(res);
+//var user = os.userInfo();
 
-fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
+//fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,3 +1,8 @@
 console.log('Starting Notes.js');
 
-module.exports.age =27;
+// module.exports.age =27;
+
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+};
