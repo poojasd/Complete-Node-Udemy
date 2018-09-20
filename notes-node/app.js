231efabb006a746cf5b3ce5f3fcefafc2e9e4b27
pooja <pooja@fenixwork.com>
2018-09-20 13:52:46 +0530

    getting input from user simple

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,17 +1,22 @@
 console.log("Starting aap.js!");
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
+
 const notes = require('./notes.js');
 
-//console.log(_.isString(true));
-//console.log(_.isString('fenix'));
-var filteredArray = _.uniq(['fenix', 1, 'fenix', 2, 1, 3, 4]);
-console.log(filteredArray);
+var command = process.argv[2];
+console.log('Command:' + command);
 
-var res = notes.add(9, -2);
-console.log(res);
-//var user = os.userInfo();
-
-//fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
+if (command === 'add'){
+    console.log('Adding new note!');
+}else if (command === 'list'){
+    console.log('Listing all notes.');
+}else if (command === 'read'){
+    console.log('Reading note.')
+}else if (command === 'remove'){
+    console.log('Removing note.');
+}else{
+    console.log('Command not recognized!');
+}
