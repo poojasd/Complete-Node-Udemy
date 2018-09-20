3e9cb24d64f2b075e85b11e9c73755d659f7ebd1
pooja <pooja@fenixwork.com>
2018-09-20 12:53:14 +0530

    added lodash used _.isString used _.uniq

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,7 +2,13 @@ console.log("Starting aap.js!");
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
+
+//console.log(_.isString(true));
+//console.log(_.isString('fenix'));
+var filteredArray = _.uniq(['fenix', 1, 'fenix', 2, 1, 3, 4]);
+console.log(filteredArray);
 
 var res = notes.add(9, -2);
 console.log(res);
